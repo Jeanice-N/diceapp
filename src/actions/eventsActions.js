22c222ef325162cb6f293047e2efa63dc312892b
Jeanice <39350965+Jeanice-N@users.noreply.github.com>
2021-08-28 19:46:53 +0100

    add search field and fetch api results

---
+++ src/actions/eventsActions.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+/**
+ * Returns events for given venue
+ * @param {string} venue - Name of venue
+ */
+export async function fetchEvents(venue) {
+  const config = { "X-Api-Key": "dHmvC0ZXzF4h1mWldfur13c6s4Ix6wCF4OTzozXC" };
+
+  const { data } = await axios.get(
+    `https://events-api.dice.fm/v1/events?filter[venues]=${venue}`,
+    { headers: config }
+  );
+
+  return data;
+}
+
+export default fetchEvents;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,8 +1,52 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
+import {
+  Typography,
+  TextField,
+  Button,
+  InputAdornment,
+  IconButton,
+} from "@material-ui/core";
+import ClearIcon from "@material-ui/icons/Clear";
+import { fetchEvents } from "../actions/eventsActions";
 
 export default function Events() {
-  return <div>hi</div>;
-}
+  const [events, setEvents] = useState([]);
+  const [venue, setVenue] = useState("");
 
-Events.propTypes = {};
+  const handleOnChange = async (e) => {
+    e.preventDefault();
+    setEvents(await fetchEvents(venue));
+  };
+
+  const handleOnClear = async (e) => {
+    setVenue("");
+    setEvents(await fetchEvents(""));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleOnChange}>
+        <TextField
+          label="Venue"
+          variant="outlined"
+          fullWidth
+          value={venue}
+          onChange={(e) => setVenue(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleOnClear}>
+                  <ClearIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" type="submit" color="primary">
+          Search
+        </Button>
+      </form>
+      <Typography>Upcoming events as Venue</Typography>
+    </div>
+  );
+}
